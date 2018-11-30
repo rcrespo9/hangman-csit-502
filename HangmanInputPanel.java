0fb1eaef08a0c17db25cfc8906b99e41a0367d1a
rudycrespo <rudy.crespo20@gmail.com>
2018-11-30 18:47:03 -0500

    paring down files

--- HangmanInputPanel.java
+++
@@ -1,9 +0,0 @@
-package rudy_prj_src;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class HangmanInputPanel extends JPanel {
-	public HangmanInputPanel() {
-	}
-}
--- HangmanWordsPanel.java
+++
@@ -1,9 +0,0 @@
-package rudy_prj_src;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class HangmanWordsPanel extends JPanel {
-	public HangmanWordsPanel() {
-	}
-}
