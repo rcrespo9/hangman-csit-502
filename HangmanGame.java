af3f67b12f661db00aeef6ffa4775e1ca6e21b64
rudycrespo <rudy.crespo20@gmail.com>
2018-12-01 23:37:27 -0500

    starting on hangman game

--- HangmanGame.java
+++ HangmanGame.java
@@ -1,5 +1,34 @@
 package rudy_prj_src;
 
-public class HangmanGame {
+import java.util.ArrayList;
 
+public class HangmanGame {
+	private final int MAX_ATTEMPTS = 6;
+	private String randomWord;
+	private ArrayList<Character> guessedLetters, wrongLetters = new ArrayList<Character>();
+	
+//	private static void setRandomWord() {
+//		
+//	}
+	
+	
+	public String getRandomWord() {
+		return this.randomWord;
+	}
+	
+	public ArrayList<Character> getWrongLetters() {
+		return this.wrongLetters;
+	}
+	
+	public int getWrongLettersCount() {
+		return this.wrongLetters.size();
+	}
+	
+	public void guessLetter(char letter) {
+		this.guessedLetters.add(letter);
+		
+		if (this.randomWord.indexOf(letter) < 0) {
+			this.wrongLetters.add(letter);
+		}
+	}
 }
---
+++ HangmanGamePanel.java
@@ -0,0 +1,10 @@
+package rudy_prj_src;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HangmanGamePanel extends JPanel {
+	public HangmanGamePanel() {		
+	}
+
+}
