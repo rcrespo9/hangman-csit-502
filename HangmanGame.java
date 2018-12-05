9bf9915a8d49ada4147c388a5152554b7595052e
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-12-05 11:47:22 -0500

    random word set, can now read from words

--- HangmanGame.java
+++ HangmanGame.java
@@ -1,19 +1,41 @@
 package rudy_prj_src;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-import javax.swing.JFrame;
+import java.io.*;
 
 public class HangmanGame {
 	private final int MAX_ATTEMPTS = 6;
-	private static String gameWord;
-	private static String randomWord = "Baseball";
+	private static String gameWord = "";
+	private static String randomWord;
+	private static ArrayList<String> wordsArrayList = new ArrayList<String>();
 	private static ArrayList<String> correctLetters = new ArrayList<String>();
 	private ArrayList<String> wrongLetters = new ArrayList<String>();
 	
-//	private static void setRandomWord() {
-//		
-//	}
+	private static void addWords() throws IOException {
+	  File file = new File("./src/rudy_prj_src/words.txt"); 
+	  
+	  BufferedReader br = null;
+	  
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+	  
+	  String word;
+	  
+	  while ((word = br.readLine()) != null) {
+		  wordsArrayList.add(word);  
+	  }
+	}
+	
+	private static void setRandomWord() {
+		int randomNum = new Random().nextInt(wordsArrayList.size());
+		
+		randomWord = wordsArrayList.get(randomNum);
+	}
 	
 	private void buildGameWord(String letter) {
 		StringBuilder sb = new StringBuilder();
@@ -37,6 +59,16 @@ public class HangmanGame {
 		}
 		
 		gameWord = sb.toString();
+	}
+	
+	public HangmanGame() {
+		try {
+			addWords();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setRandomWord();
 	}
 	
 	public String getGameWord() {
